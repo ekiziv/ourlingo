cfbe3c76fc281db9bc5ff96bc4e17d3b78b06292
sachi <sachi1210@me.com>
2018-08-21 14:56:38 -0300

    Delete filed from business

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,6 +2,16 @@ class BusinessesController < ApplicationController
 
   def index
     @businesses = Business.all
+
+    @businesses = Business.where.not(latitude: nil, longitude: nil)
+
+    @markers = @businesses.map do |business|
+      {
+        lat: businesses.latitude,
+        lng: businesses.longitude,
+
+      }
+    end
   end
 
   def show
