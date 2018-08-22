6dc980f458aabc5b889b1d4d32e6736986858df1
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2018-08-22 07:58:20 -0300

    Start with Google API

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -12,6 +12,10 @@ class BusinessesController < ApplicationController
   end
 
   def show
+    # This is the case we have it in our datebase
     @business = Business.find(params[:id])
+    @client = GooglePlaces::Client.new(ENV['GOOGLE_API_SERVER_KEY'])
+    @info = @client.spots(@business.latitude, @business.longitude,  { name: @business.name, address: @business.address, detail: true}).first
+    @url = @info.photos[0].fetch_url(760)
   end
 end
