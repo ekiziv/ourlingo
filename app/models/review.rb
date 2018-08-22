5abd8ab9d3a46d8b330bdf6668f203fd0f0cdaec
Steve Knight <stevejknight@hotmail.co.uk>
2018-08-22 15:20:42 -0300

    removed business and review controllers, pages, routes.

--- app/models/review.rb
+++
@@ -1,8 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :user
-  belongs_to :business
-
-  validates :description, presence: true
-  validates :english_rating, inclusion: { in: [1, 2, 3, 4, 5] }
-  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_145406) do
+ActiveRecord::Schema.define(version: 2018_08_21_134207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "businesses", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "description"
-    t.integer "english_rating"
-    t.integer "rating"
-    t.bigint "user_id"
-    t.bigint "business_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["business_id"], name: "index_reviews_on_business_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +28,4 @@ ActiveRecord::Schema.define(version: 2018_08_21_145406) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reviews", "businesses"
-  add_foreign_key "reviews", "users"
 end
