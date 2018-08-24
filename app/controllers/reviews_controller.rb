a45645c49994e540382553370aab64ecd9e5c5e7
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2018-08-24 13:37:27 -0300

    Rollback to the working localhost

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
     query = params[:input_address]
     formatted_query = URI.encode(query)
     # location_helper = "location=#{lat},#{lon}"
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?input=#{formatted_query}&inputtype=textquery&fields=photos,formatted_address,name,rating&key=#{ENV['GOOGLE_API_BROWSER_KEY']}"
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?input=#{formatted_query}&inputtype=textquery&fields=photos,formatted_address,name,rating&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
     # url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?#{location_helper}&radius=5500&keyword=#{formatted_query}&fields=photos,formatted_address,name,rating&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
     places = HTTParty.get(url)
     @candidates = places["results"]
@@ -21,7 +21,7 @@ class ReviewsController < ApplicationController
 
   def show
     @place_id = params[:id]
-    url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{@place_id}&fields=formatted_address,name,photos,rating,formatted_phone_number&key=#{ENV['GOOGLE_API_BROWSER_KEY']}"
+    url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{@place_id}&fields=formatted_address,name,photos,rating,formatted_phone_number&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
     place_info = HTTParty.get(url)
     @result = place_info["result"]
 
