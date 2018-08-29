2d0d24b9ae3c8dfbd7bce6e4d2f928b4ccb680e8
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2018-08-29 10:03:32 -0300

    Correct Autocomplete, center the review boxes

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,10 +1,19 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var inputAddress = document.getElementById('input_address');
+    var inputAddr = document.getElementById('input_addr');
 
     if (inputAddress) {
       var autocomplete = new google.maps.places.SearchBox(inputAddress);
       google.maps.event.addDomListener(inputAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+    if (inputAddr) {
+      var autocomplete = new google.maps.places.SearchBox(inputAddr);
+      google.maps.event.addDomListener(inputAddr, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
