cb0ee1a44ff957f6860a5903bd3c3d27ebd8db12
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2018-08-29 11:43:22 -0300

    Correct the enter search logic

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,10 +1,19 @@
 const search = () => {
-  const form = document.getElementById('search_place')
-  const input = document.getElementById('input_address')
+  const form = document.getElementById('search_place');
+  const input = document.getElementById('input_address');
+
+  const form_navbar = document.getElementById('search_navbar');
+  const input_navbar = document.getElementById('input_addr');
 
   input.addEventListener('keydown', event => {
     if (event.keyCode == 13) {
       form.submit();
+    }
+  });
+
+  input_navbar.addEventListener('keydown', event => {
+    if (event.keyCode == 13) {
+      form_navbar.submit();
     }
   });
 }
